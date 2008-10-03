b7daacbe8cf6163ba0856de74c5b5e9738f58d77
Drew Hess <dhess-src@bothan.net>
2008-10-02 17:31:52 -0700

    Added complete set of tests for importing issues.

--- tests/test_import.py
+++ tests/test_import.py
@@ -989,6 +989,212 @@ class TestImport(unittest.TestCase):
 
         self.failUnlessEqual(len(rows), 0)
 
-        
+    def test_import_issues_issue_code(self):
+        """Importing issues should fill issue_code table."""
+        filings = list(lobbyists.parse_filings(util.testpath('issues.xml')))
+        con = sqlite3.connect(':memory:')
+        con.executescript(util.sqlscript('filings.sql'))
+        cur = con.cursor()
+        self.failUnless(lobbyists.import_filings(cur, filings))
+
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM issue_code")
+        rows = list(cur)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'ENERGY/NUCLEAR')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'TRANSPORTATION')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'IMMIGRATION')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'AGRICULTURE')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'TRADE (DOMESTIC/FOREIGN)')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'EDUCATION')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'ROADS/HIGHWAY')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'HOMELAND SECURITY')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'DEFENSE')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'BANKING')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'REAL ESTATE/LAND USE/CONSERVATION')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'FINANCIAL INSTITUTIONS/INVESTMENTS/SECURITIES')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'FOREIGN RELATIONS')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'LAW ENFORCEMENT/CRIME/CRIMINAL JUSTICE')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'FAMILY ISSUES/ABORTION/ADOPTION')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'HEALTH ISSUES')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'MEDICARE/MEDICAID')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'WELFARE')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'BUDGET/APPROPRIATIONS')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'TAXATION/INTERNAL REVENUE CODE')
+
+        row = rows.pop()
+        self.failUnlessEqual(row['code'],
+                             'INSURANCE')
+
+        self.failUnlessEqual(len(rows), 0)
+
+    def test_import_filings_to_issues(self):
+        """Ensure issues are matched up with filings in the database."""
+        filings = list(lobbyists.parse_filings(util.testpath('issues.xml')))
+        con = sqlite3.connect(':memory:')
+        con.executescript(util.sqlscript('filings.sql'))
+        cur = con.cursor()
+        self.failUnless(lobbyists.import_filings(cur, filings))
+
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM filing_issues")
+        rows = list(cur)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '79E53F91-8C5F-44AD-909D-032AA25D5B00')
+        self.failUnlessEqual(row['issue'], 23)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '79E53F91-8C5F-44AD-909D-032AA25D5B00')
+        self.failUnlessEqual(row['issue'], 22)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '79E53F91-8C5F-44AD-909D-032AA25D5B00')
+        self.failUnlessEqual(row['issue'], 21)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '79E53F91-8C5F-44AD-909D-032AA25D5B00')
+        self.failUnlessEqual(row['issue'], 20)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '79E53F91-8C5F-44AD-909D-032AA25D5B00')
+        self.failUnlessEqual(row['issue'], 19)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '79E53F91-8C5F-44AD-909D-032AA25D5B00')
+        self.failUnlessEqual(row['issue'], 18)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '79E53F91-8C5F-44AD-909D-032AA25D5B00')
+        self.failUnlessEqual(row['issue'], 17)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '05804BE5-57C9-41BF-97B2-0120826D4393')
+        self.failUnlessEqual(row['issue'], 16)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '05804BE5-57C9-41BF-97B2-0120826D4393')
+        self.failUnlessEqual(row['issue'], 15)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], '05804BE5-57C9-41BF-97B2-0120826D4393')
+        self.failUnlessEqual(row['issue'], 14)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'F56492FC-4FBD-4824-83E1-0004B30F0519')
+        self.failUnlessEqual(row['issue'], 13)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'A55002C7-78C4-41BA-A6CA-01FCF7650116')
+        self.failUnlessEqual(row['issue'], 12)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'A55002C7-78C4-41BA-A6CA-01FCF7650116')
+        self.failUnlessEqual(row['issue'], 11)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'A55002C7-78C4-41BA-A6CA-01FCF7650116')
+        self.failUnlessEqual(row['issue'], 10)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'A55002C7-78C4-41BA-A6CA-01FCF7650116')
+        self.failUnlessEqual(row['issue'], 9)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'A55002C7-78C4-41BA-A6CA-01FCF7650116')
+        self.failUnlessEqual(row['issue'], 8)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'A55002C7-78C4-41BA-A6CA-01FCF7650116')
+        self.failUnlessEqual(row['issue'], 7)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'D1C9DB2A-AE4F-4FED-9BCB-024C8373813E')
+        self.failUnlessEqual(row['issue'], 6)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'D1C9DB2A-AE4F-4FED-9BCB-024C8373813E')
+        self.failUnlessEqual(row['issue'], 5)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'D1C9DB2A-AE4F-4FED-9BCB-024C8373813E')
+        self.failUnlessEqual(row['issue'], 4)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'D1C9DB2A-AE4F-4FED-9BCB-024C8373813E')
+        self.failUnlessEqual(row['issue'], 3)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'D1C9DB2A-AE4F-4FED-9BCB-024C8373813E')
+        self.failUnlessEqual(row['issue'], 2)
+
+        row = rows.pop()
+        self.failUnlessEqual(row['filing'], 'D1C9DB2A-AE4F-4FED-9BCB-024C8373813E')
+        self.failUnlessEqual(row['issue'], 1)
+
+        self.failUnlessEqual(len(rows), 0)
+
+
 if __name__ == '__main__':
     unittest.main()
